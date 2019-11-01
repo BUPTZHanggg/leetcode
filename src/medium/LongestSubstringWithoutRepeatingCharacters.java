e57c789944544c7bebb076bdcbaa28e324163b39
zhaohang <zhaohang02@megvii.com>
2019-11-01 18:22:12 +0800

    commit

---
+++ src/medium/LongestSubstringWithoutRepeatingCharacters.java
@@ -0,0 +1,17 @@
+package medium;
+
+public class LongestSubstringWithoutRepeatingCharacters {
+
+    public int lengthOfLongestSubstring(String s) {
+        int len = 0;
+        int beginIndex = 0;
+        for (int i = 0;i < s.length();i++){
+
+        }
+        return 0;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}

726d302be25a7f4a3b33f2af98f85a9bff73421e
Hang <ZhaoHang@bupt.edu.cn>
2019-11-03 12:03:15 +0800

    20191103

--- src/medium/LongestSubstringWithoutRepeatingCharacters.java
+++ src/medium/LongestSubstringWithoutRepeatingCharacters.java
@@ -2,16 +2,30 @@ package medium;
 
 public class LongestSubstringWithoutRepeatingCharacters {
 
-    public int lengthOfLongestSubstring(String s) {
-        int len = 0;
+    public static int lengthOfLongestSubstring(String s) {
+        int tmpLen = 0;//每次循环的长度
+        int result = 0;//记录最长的字串长度
         int beginIndex = 0;
         for (int i = 0;i < s.length();i++){
-
+            String tmpStr = s.substring(beginIndex, i);
+            int indexTmp = tmpStr.indexOf(s.charAt(i));
+            if (indexTmp ==  -1){//说明该char在这一次的长度叠加中第一次出现
+                tmpLen++;
+                if (tmpLen > result){
+                    result = tmpLen;
+                }
+            }else {
+                beginIndex = indexTmp + 1 + beginIndex;
+                if (tmpLen > result){
+                    result = tmpLen;
+                }
+                tmpLen = i - beginIndex + 1;
+            }
         }
-        return 0;
+        return result;
     }
 
     public static void main(String[] args) {
-
+        System.out.println(lengthOfLongestSubstring("abcbaddfghj"));
     }
 }
